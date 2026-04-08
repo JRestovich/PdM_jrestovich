fb1371c90966a0f1b1391c6a1edabd60df5c8dd5
JRestovich <joaquinrestovich96@mail.com>
2026-04-08 00:55:55 -0300

    implement uart methods

--- practica_5/Drivers/API/Src/API_uart.c
+++ practica_5/Drivers/API/Src/API_uart.c
@@ -11,7 +11,12 @@
 #include "string.h"
 
 #define TIMEOUT_UART2 10
+#define MIN_SIZE 1
+#define MAX_SIZE 256
 UART_HandleTypeDef huart2;
+
+static HAL_StatusTypeDef uartStatus = false;
+static bool_t rxReady = false;
 
 bool_t uartInit()
 {
@@ -29,15 +34,15 @@ bool_t uartInit()
 	} else {
 		char buffer[200];
 		sprintf(buffer,
-				"UART Config:\r\n"
-				"BaudRate: %lu\r\n"
-				"WordLength: %d\r\n"
-				"StopBits: %d\r\n"
-				"Parity: %d\r\n",
-				huart2.Init.BaudRate,
-				huart2.Init.WordLength,
-				huart2.Init.StopBits,
-				huart2.Init.Parity);
+		        "UART Config:\r\n"
+		        "BaudRate: %lu\r\n"
+		        "WordLength: %lu\r\n"
+		        "StopBits: %lu\r\n"
+		        "Parity: %lu\r\n",
+		        huart2.Init.BaudRate,
+		        huart2.Init.WordLength,
+		        huart2.Init.StopBits,
+		        huart2.Init.Parity);
 
 		uartSendStringSize((uint8_t*)buffer, strlen(buffer));
 	    return true;
@@ -49,6 +54,7 @@ void uartSendString(uint8_t * pstring)
 	if (pstring == NULL) {
 		return;
 	}
+	uartStatus = HAL_UART_Transmit(&huart2, pstring, strlen((char*)pstring), TIMEOUT_UART2);
 }
 
 void uartSendStringSize(uint8_t * pstring, uint16_t size)
@@ -56,11 +62,20 @@ void uartSendStringSize(uint8_t * pstring, uint16_t size)
 	if (pstring == NULL) {
 		return;
 	}
+	if (size < MIN_SIZE || size >= MAX_SIZE) {
+		return;
+	}
+	uartStatus = HAL_UART_Transmit(&huart2, pstring, size, TIMEOUT_UART2);
 }
 
 void uartReceiveStringSize(uint8_t * pstring, uint16_t size)
 {
+	rxReady = false;
 	if (pstring == NULL) {
 		return;
+	}
+
+	if (HAL_UART_Receive (&huart2, pstring, size,TIMEOUT_UART2) == HAL_OK){
+		rxReady = true;
 	}
 }
