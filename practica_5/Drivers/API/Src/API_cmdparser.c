cde821a99f680f190314f1dbc57b07c7774acbf0
JRestovich <joaquinrestovich96@mail.com>
2026-04-08 01:24:12 -0300

    basic echo

--- practica_5/Drivers/API/Src/API_cmdparser.c
+++ practica_5/Drivers/API/Src/API_cmdparser.c
@@ -6,13 +6,19 @@
  */
 
 #include "API_cmdparser.h"
+#include "API_uart.h"
 
 void cmdParserInit(void) {
-
+	uartInit();
 }
 
 void cmdPoll(void) {
+	uint8_t rx;
+	uartReceiveStringSize(&rx, 1);
 
+	if (uartReceiveOk()) {
+		uartSendStringSize(&rx, 1);
+	}
 }
 
 void cmdPrintHelp(void) {
