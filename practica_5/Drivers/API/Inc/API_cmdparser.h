a039b8ec8996aacaace01c756829b15058a2b2f7
JRestovich <joaquinrestovich96@mail.com>
2026-04-08 02:24:13 -0300

    first mef implementation

--- practica_5/Drivers/API/Inc/API_cmdparser.h
+++ practica_5/Drivers/API/Inc/API_cmdparser.h
@@ -8,13 +8,21 @@
 #ifndef API_INC_API_CMDPARSER_H_
 #define API_INC_API_CMDPARSER_H_
 
-typedef enum{
+typedef enum {
 	CMD_OK=0,
 	CMD_ERR_OVERFLOW,
 	CMD_ERR_SYNTAX,
 	CMD_ERR_UNKNOWN,
 	CMD_ERR_ARG
 } cmd_status_t;
+
+typedef enum {
+	CMD_IDLE=0,
+	CMD_RECEIVING,
+	CMD_PROCESS,
+	CMD_EXEC,
+	CMD_ERROR
+} cmdParser_state_t;
 
 
 void cmdParserInit(void);
--- practica_5/Drivers/API/Src/API_cmdparser.c
+++ practica_5/Drivers/API/Src/API_cmdparser.c
@@ -8,19 +8,89 @@
 #include "API_cmdparser.h"
 #include "API_uart.h"
 
+#define BUFFER_SIZE 256
+
+static cmdParser_state_t state;
+static cmd_status_t status = CMD_OK;
+
+static uint8_t buffer[BUFFER_SIZE];
+static uint8_t index = 0;
+
+static void cmdProcessLine(void);
+
 void cmdParserInit(void) {
-	uartInit();
+	if (uartInit()) {
+		state = CMD_IDLE;
+	} else {
+		state = CMD_ERROR;
+	}
 }
 
 void cmdPoll(void) {
 	uint8_t rx;
 	uartReceiveStringSize(&rx, 1);
 
-	if (uartReceiveOk()) {
-		uartSendStringSize(&rx, 1);
+	switch (state) {
+	case CMD_IDLE:
+		// Return there isn't a new data
+		if (!uartReceiveOk()) {
+			return;
+		}
+
+		if (rx != '\n' && rx != '\r') {
+			index = 0;
+			buffer[index] = rx;
+			state = CMD_RECEIVING;
+		}
+		break;
+
+	case CMD_RECEIVING:
+		// Return there isn't a new data
+		if (!uartReceiveOk()) {
+			return;
+		}
+		index++;
+		if (index >= BUFFER_SIZE) {
+			state = CMD_ERROR;
+			status = CMD_ERR_OVERFLOW;
+		} else if (rx == '\n' || rx == '\r') {
+			state = CMD_PROCESS;
+			buffer[index] = rx;
+		} else {
+			buffer[index] = rx;
+		}
+		break;
+
+	case CMD_PROCESS:
+		cmdProcessLine();
+		break;
+
+	case CMD_EXEC:
+		// do action
+		state = CMD_IDLE;
+		break;
+
+	case CMD_ERROR:
+		// print error msg
+		state = CMD_IDLE;
+		break;
+
+	default:
+		break;
 	}
 }
 
 void cmdPrintHelp(void) {
 
+}
+
+
+void cmdProcessLine(void) {
+	if (buffer[0] == '#') {
+		state = CMD_ERROR;
+		status = CMD_OK;
+	} else if (buffer[0] == '/' && buffer[1] == '/') {
+		state = CMD_ERROR;
+		status = CMD_OK;
+	}
 }
