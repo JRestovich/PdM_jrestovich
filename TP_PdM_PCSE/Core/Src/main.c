6d41d24fe116923dd4765c324f16634c04d45733
JRestovich <joaquinrestovich96@mail.com>
2026-04-10 20:49:41 -0300

    delete commented code and leave a simple blink

--- TP_PdM_PCSE/Core/Src/main.c
+++ TP_PdM_PCSE/Core/Src/main.c
@@ -15,54 +15,23 @@
   *
   ******************************************************************************
   */
-/* USER CODE END Header */
+
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include <stdbool.h>
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
-
-/* USER CODE END Includes */
-
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
-
-/* USER CODE END PTD */
-
 /* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
 #define DEBOUNCE_MS 30U
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
 UART_HandleTypeDef huart2;
-
-/* USER CODE BEGIN PV */
-
-/* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
-/* USER CODE BEGIN PFP */
-
-/* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
-static uint32_t timesMs[] = {200, 500};
-static uint8_t  timeIndex = 0;
-static uint32_t lastDebounceTick = 0;
-static bool pressed = false;
-
-/* USER CODE END 0 */
 
 /**
   * @brief  The application entry point.
@@ -70,58 +39,15 @@ static bool pressed = false;
   */
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
   SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART2_UART_Init();
-  /* USER CODE BEGIN 2 */
 
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
   while (1)
   {
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
-
 	  HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
-	  //HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
-	  HAL_Delay(timesMs[timeIndex]);
-
-	  if (!HAL_GPIO_ReadPin(B1_GPIO_Port, B1_Pin) && !pressed){
-		  timeIndex++;
-		  pressed = true;
-		  lastDebounceTick = HAL_GetTick();
-		  if (timeIndex >= (sizeof(timesMs) / sizeof(timesMs[0]))) {
-			  timeIndex = 0;
-		  }
-	  }
-
-	  uint32_t now = HAL_GetTick();
-	  if (pressed && now - lastDebounceTick > DEBOUNCE_MS) {
-		  pressed = false;
-	  }
+	  HAL_Delay(500);
 
   }
   /* USER CODE END 3 */
@@ -258,20 +184,3 @@ void Error_Handler(void)
   }
   /* USER CODE END Error_Handler_Debug */
 }
-
-#ifdef  USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
-}
-#endif /* USE_FULL_ASSERT */
