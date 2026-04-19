987f30da4b30733318e78154afc03ec7e3b65c52
JRestovich <joaquinrestovich96@mail.com>
2026-04-18 23:20:21 -0300

    move includes to API_LCD16x2

--- TP_PdM_PCSE/Drivers/API/Src/API_LCD16x2.c
+++ TP_PdM_PCSE/Drivers/API/Src/API_LCD16x2.c
@@ -6,14 +6,54 @@
  */
 
 #include "API_LCD16x2.h"
-#include "API_lcd.h"
 #include "API_LCD16x2_port.h"
 #include "API_delay.h"
 #include <stdio.h>
 #include <string.h>
 
-static const char blankDisplay[] = "                "; // 16 spaces 
+#define LCD_DIR 0x27
 
+#define DATOS 1
+#define CONTROL 0
+
+#define CLR_LCD 1
+#define RETURN_HOME (1<<1)
+#define ENTRY_MODE (1<<2)
+#define DISPLAY_CONTROL (1<<3)
+#define CURSOR_DISPLAY_SHIFT (1<<4)
+#define FUNTION_SET (1<<5)
+#define SET_CGRAM (1<<6)
+#define SET_DDRAM (1<<7)
+/************************************/
+#define DISPLAY_ON (1<<2)
+#define SHIFT_RIGHT (1<<2)
+#define DISPLAY_SHIFT (1<<3)
+#define CURSOR_ON (1<<1)
+#define CURSOR_BLINK 1
+#define AUTOINCREMENT (1<<1)
+#define _4BIT_MODE 0x28
+
+//DEMORAS NECESARIAS
+
+#define DELAY20ms 20
+#define DELAY10ms 10
+#define DELAY2ms  2
+#define DELAY1ms  1
+#define DelayTime 1
+
+//MANEJO DE NIBBLES
+#define HIGH_NIBBLE 0xf0
+#define LOW_NIBBLE 4
+
+//LINEAS DEL LCD
+#define LINEA1 0x80
+#define LINEA2 0xc0
+
+//COMANDOS DE INICIALIZACIÓN
+#define COMANDO_INI1 0x30
+#define COMANDO_INI2 0x20
+
+static const char blankDisplay[] = "                "; // 16 spaces
 static const uint8_t LCD_INIT_CMD[]={ _4BIT_MODE,
                                       DISPLAY_CONTROL+DISPLAY_ON,
                                       CLR_LCD,
